b87ba0873d134c4188fbc7a3f750b7697f1363f9
Hackerstudent98 <lacaotungchao@gmail.com>
2017-06-30 22:54:07 +0700

    change theme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">Admin</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
     </div>
-</nav>
+</div>
 @endsection
