f4b0b2a51affa7e4b33d18fdfbe09c82d68986fd
Hackerstudent98 <lacaotungchao@gmail.com>
2017-06-30 22:23:42 +0700

    change theme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                   Hellop
+                   Hellopaa
                 </div>
             </div>
         </div>
